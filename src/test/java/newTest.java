c50df9cfcbedf8cf5952831373ad307d03f0d2b3
OlegYozh <59981355+OlegYozh@users.noreply.github.com>
2022-07-10 18:15:23 +0300

    Update newTest.java

--- src/test/java/newTest.java
+++ src/test/java/newTest.java
@@ -11,8 +11,8 @@ public class newTest {
 
     @Test
     public void test0() {
-        String companyName = StringEscapeUtils.escapeJava("Альбатрос").toLowerCase(Locale.ROOT);
-        String companyType = StringEscapeUtils.escapeJava("ООО").toLowerCase(Locale.ROOT);
+        String companyName = StringEscapeUtils.escapeJava("ГЂГ«ГјГЎГ ГІГ°Г®Г±").toLowerCase(Locale.ROOT);
+        String companyType = StringEscapeUtils.escapeJava("ГЋГЋГЋ").toLowerCase(Locale.ROOT);
         String emailOwner = "aa+1@mail.com";
         String users = "[\"test_cu_11@mail.com\",\"test_dev@mail.com\",\"ivan@noibiz.com\"]";
         String reqBody = "{\n" +
@@ -38,7 +38,7 @@ public class newTest {
                 "    <soapenv:Body>\n" +
                 "        <wrap:CreateCompany>\n" +
                 "            <company_name>123</company_name>\n" +
-                "            <company_type>ООО</company_type>\n" +
+                "            <company_type>РћРћРћ</company_type>\n" +
                 "            <email_owner>aa+1@mail.com</email_owner>\n" +
                 "            <company_users soapenc:arrayType=\"xsd:array[]\">\n" +
                 "                <item>test_cu_11@mail.com</item>\n" +
@@ -52,6 +52,6 @@ public class newTest {
         String respBodyActual = given().when().body(reqBody).post("http://users.bugred.ru/tasks/soap/WrapperSoapServer.php").then().assertThat().statusCode(HttpStatus.SC_OK).extract().asPrettyString();
         System.out.println(respBodyActual);
 
-        Assert.assertTrue(respBodyActual.contains("<message xsi:type=\"xsd:string\">Компания успешно создана!</message>"));
+        Assert.assertTrue(respBodyActual.contains("<message xsi:type=\"xsd:string\">ГЉГ®Г¬ГЇГ Г­ГЁГї ГіГ±ГЇГҐГёГ­Г® Г±Г®Г§Г¤Г Г­Г !</message>"));
     }
 }
