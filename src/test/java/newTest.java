c156b3267486f6dc8350c459e3db4c1b4a86d6f1
OlegYozh <59981355+OlegYozh@users.noreply.github.com>
2022-07-10 21:12:24 +0300

    Update newTest.java

--- src/test/java/newTest.java
+++ src/test/java/newTest.java
@@ -1,3 +1,4 @@
+
 import groovy.json.StringEscapeUtils;
 import org.apache.http.HttpStatus;
 import org.junit.Assert;
@@ -11,8 +12,8 @@ public class newTest {
 
     @Test
     public void test0() {
-        String companyName = StringEscapeUtils.escapeJava("Тестовый").toLowerCase(Locale.ROOT);
-        String companyType = StringEscapeUtils.escapeJava("ООО").toLowerCase(Locale.ROOT);
+        String companyName = StringEscapeUtils.escapeJava("Àëüáàòðîñ").toLowerCase(Locale.ROOT);
+        String companyType = StringEscapeUtils.escapeJava("ÎÎÎ").toLowerCase(Locale.ROOT);
         String emailOwner = "aa+1@mail.com";
         String users = "[\"test_cu_11@mail.com\",\"test_dev@mail.com\",\"ivan@noibiz.com\"]";
         String reqBody = "{\n" +
@@ -38,7 +39,7 @@ public class newTest {
                 "    <soapenv:Body>\n" +
                 "        <wrap:CreateCompany>\n" +
                 "            <company_name>123</company_name>\n" +
-                "            <company_type>ООО</company_type>\n" +
+                "            <company_type>ÎÎÎ</company_type>\n" +
                 "            <email_owner>aa+1@mail.com</email_owner>\n" +
                 "            <company_users soapenc:arrayType=\"xsd:array[]\">\n" +
                 "                <item>test_cu_11@mail.com</item>\n" +
@@ -52,6 +53,6 @@ public class newTest {
         String respBodyActual = given().when().body(reqBody).post("http://users.bugred.ru/tasks/soap/WrapperSoapServer.php").then().assertThat().statusCode(HttpStatus.SC_OK).extract().asPrettyString();
         System.out.println(respBodyActual);
 
-        Assert.assertTrue(respBodyActual.contains("<message xsi:type=\"xsd:string\">Компания успешно создана!</message>"));
+        Assert.assertTrue(respBodyActual.contains("<message xsi:type=\"xsd:string\">Êîìïàíèÿ óñïåøíî ñîçäàíà!</message>"));
     }
 }
